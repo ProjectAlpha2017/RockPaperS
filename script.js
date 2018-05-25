f0865a232de570ee3ac5f2a79442cf9eade4d777
wayne <hedgehog1982@gmail.com>
2018-05-25 17:15:02 +0100

    Capitilised letters

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let score = 0;
-let options = ['rock', 'paper', 'scissors'];
+let options = ['Rock', 'Paper', 'Scissors'];
 
 const updateDisplay = (score, player, computerPlayer) => {
     document.getElementById("score").innerHTML = score;
@@ -11,9 +11,9 @@ const buttonClick = event => {
     console.log(event); // log where clicked
     let clickedButton = event.target.id;
     if (
-        clickedButton === 'rock' ||
-        clickedButton === 'scissors' ||
-        clickedButton === 'paper'
+        clickedButton === 'Rock' ||
+        clickedButton === 'Scissors' ||
+        clickedButton === 'Paper'
     ) {
         console.log('clicked a button');
         let computerPlayer = options[Math.floor(Math.random() * 2)]; //computer player is picked between 0 and 2
@@ -21,21 +21,21 @@ const buttonClick = event => {
         console.log(computerPlayer, player);
         //win events
         if (
-            (computerPlayer === 'rock' && player === 'paper') ||
-            (computerPlayer === 'paper' && player === 'scissors') ||
-            (computerPlayer === 'scissors' && player === 'rock')
+            (computerPlayer === 'Rock' && player === 'Paper') ||
+            (computerPlayer === 'Paper' && player === 'Scissors') ||
+            (computerPlayer === 'Scissors' && player === 'Rock')
         ) {
             console.log("WON")
             score += 5
         
         //lose events
         } else if (
-            (player === 'rock' && computerPlayer === 'paper') ||
-            (player === 'paper' && computerPlayer === 'scissors') ||
-            (player === 'scissors' && computerPlayer === 'rock')
+            (player === 'Rock' && computerPlayer === 'Paper') ||
+            (player === 'Paper' && computerPlayer === 'Scissors') ||
+            (player === 'Scissors' && computerPlayer === 'Rock')
         ) {
             console.log("lost")
-            score = 2
+            score -= 2
         } else {
             console.log("draw")
         }
