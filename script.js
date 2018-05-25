65b95d2c9c6dcd4140db4f5bd4606145596e9a80
wayne <hedgehog1982@gmail.com>
2018-05-25 16:48:17 +0100

    added logic for win/ lose

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 let score = 0;
-let options = ["rock", "paper", "scissors"]
+let options = ['rock', 'paper', 'scissors'];
 
-
-const buttonClick = (event) => {
-    console.log(event)  // log where clicked
-    let clickedButton = event.target.id
-    if (clickedButton === "rock"  || clickedButton === "scissors" || clickedButton ==="paper"){
-        console.log("clicked a button")
-        let computerPlayer = options[Math.floor(Math.random * 2)]; //computer player is picked between 0 and 2
+const buttonClick = event => {
+    console.log(event); // log where clicked
+    let clickedButton = event.target.id;
+    if (
+        clickedButton === 'rock' ||
+        clickedButton === 'scissors' ||
+        clickedButton === 'paper'
+    ) {
+        console.log('clicked a button');
+        let computerPlayer = options[Math.floor(Math.random() * 2)]; //computer player is picked between 0 and 2
         let player = clickedButton;
-    }
-}
+        console.log(computerPlayer, player);
+        //win events
+        if (
+            (computerPlayer === 'rock' && player === 'paper') ||
+            (computerPlayer === 'paper' && player === 'scissors') ||
+            (computerPlayer === 'scissors' && player === 'rock')
+        ) {
+            console.log("WON")
+            score += 5
 
-document.addEventListener("click", buttonClick) 
+        } else if (
+            (player === 'rock' && computerPlayer === 'paper') ||
+            (player === 'paper' && computerPlayer === 'scissors') ||
+            (player === 'scissors' && computerPlayer === 'rock')
+        ) {
+            console.log("lost")
+            score = 2
+        }
+    }
+};
+
+document.addEventListener('click', buttonClick);
