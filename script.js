0e9c442bd9f736ab50f4b614f551676024efc92b
wayne <hedgehog1982@gmail.com>
2018-05-25 16:37:47 +0100

    added basic script

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 let score = 0;
+let options = ["rock", "paper", "scissors"]
 
 
 const buttonClick = (event) => {
-    console.log(event)
+    console.log(event)  // log where clicked
+    let clickedButton = event.target.id
+    if (clickedButton === "rock"  || clickedButton === "scissors" || clickedButton ==="paper"){
+        console.log("clicked a button")
+        let computerPlayer = options[Math.floor(Math.random * 2)]; //computer player is picked between 0 and 2
+        let player = clickedButton;
+    }
 }
 
 document.addEventListener("click", buttonClick) 
